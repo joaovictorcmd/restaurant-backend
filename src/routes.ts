b79534fe634bb661a8161d6e994e3169e8bac94e
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2024-12-18 15:14:40 -0300

    :sparkles: feat: Add category functionality

--- src/routes.ts
+++ src/routes.ts
@@ -5,11 +5,26 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { isAuthenticatedMiddleware } from "./middlewares/isAuthenticatedMiddleware";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 const router = Router();
 
+// USER ROUTES
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticatedMiddleware, new DetailUserController().handle);
+
+// CATEGORY ROUTES
+router.post(
+  "/category",
+  isAuthenticatedMiddleware,
+  new CreateCategoryController().handle
+);
+router.get(
+  "/category",
+  isAuthenticatedMiddleware,
+  new ListCategoryController().handle
+);
 
 export { router };
